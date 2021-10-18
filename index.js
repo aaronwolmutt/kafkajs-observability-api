476f7410cc1d384cca0c5058386531c85487678e
aaron.wolmutt <awolmutt17@gmail.com>
2021-10-18 12:07:52 -0500

    added logging

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import {adminApiV1} from './controllers/admin_api_v1';
 import express from 'express';
+import {Kafka} from 'kafkajs';
 import {v4 as uuidv4} from 'uuid';
+const winston = require('winston');
 
+if (!process.env.NODE_ENV) {
+  logger.error('NODE_ENV not set, exiting');
+  process.exit(1);
+}
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'dev' ? 'debug' : 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+if (process.env.BROKERS) {
+  const kafka = new Kafka({
+    clientId: 'observability-js',
+    brokers: process.env.BROKERS.split(','),
+  });
+}
 
 export const app = express();
 const port = process.env.PORT || 3000;
-
-if (!process.env.NODE_ENV) {
-  console.error('environment not set, exiting');
-  process.exit(1);
-}
 
 app.all('/*', async (req, res, next) => {
   res.setHeader('X-Correlation-ID', uuidv4().toString());
@@ -23,5 +39,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`express listening on port ${port}`);
+  logger.info(`express listening on port ${port}`);
 });
